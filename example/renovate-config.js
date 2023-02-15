28a4ce5084c333c1ed5877a0de141053c9d7e672
keerthanakt1 <44190748+keerthanakt1@users.noreply.github.com>
2023-02-15 14:43:03 +0530

    Update renovate-config.js

--- example/renovate-config.js
+++ example/renovate-config.js
@@ -1,13 +1,13 @@
 module.exports = {
   platform: 'azure',
   endpoint: 'https://dev.azure.com/keerthanakt94/',
-  token: 'bnRuMmJmYW42cmg1aW9saTZ5bXB2c2Zkd2RqN2J4Z2F2NzVxZng0bGUzcTVvNnR0NTJiYQ==',
+  token: process.env.TOKEN,
   hostRules: [
     {
       hostType: 'npm',
       matchHost: 'pkgs.dev.azure.com',
       username: 'keerthanakt94',
-      password: 'bnRuMmJmYW42cmg1aW9saTZ5bXB2c2Zkd2RqN2J4Z2F2NzVxZng0bGUzcTVvNnR0NTJiYQ==',
+      password: process.env.TOKEN,
     },
   ],
   repositories: ['keerthanakt94/Main'],
