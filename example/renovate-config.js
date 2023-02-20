f44a7742f878990dc62853d1f247eee6608f4720
ajit_d_darade <74593429+ajitdarade@users.noreply.github.com>
2023-02-20 17:47:37 +0530

    Update renovate-config.js

--- example/renovate-config.js
+++ example/renovate-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   platform: 'azure',
-  endpoint: 'https://dev.azure.com/keerthanakt94/',
+  endpoint: 'https://dev.azure.com/ajitddarade/',
   token: process.env.TOKEN,
   hostRules: [
     {
       hostType: 'npm',
       matchHost: 'pkgs.dev.azure.com',
-      username: 'keerthanakt94',
+      username: 'ajitddarade',
       password: process.env.TOKEN,
     },
   ],
-  repositories: ['keerthanakt94/Main'],
+  repositories: ['ajitddarade/Main'],
 };
