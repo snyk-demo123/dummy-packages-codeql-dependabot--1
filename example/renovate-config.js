e4e717b7bd89be70e60932161d2cd200a5fe9497
ajit_d_darade <74593429+ajitdarade@users.noreply.github.com>
2023-02-21 12:12:26 +0530

    Update renovate-config.js

--- example/renovate-config.js
+++ example/renovate-config.js
@@ -1,14 +1,27 @@
+#module.exports = {
+#  platform=azure,
+#  endpoint: 'https://dev.azure.com/ajitddarade/',
+#  token: process.env.TOKEN,
+#  hostRules: [
+#    {
+#      hostType: 'npm',
+#      matchHost: 'pkgs.dev.azure.com',
+#      username: 'ajitddarade',
+#      password: process.env.TOKEN,
+#    },
+#  ],
+#  repositories: ['snyk-demo123/dummy-packages-codeql-dependabot--1'],
+#};
+#
 module.exports = {
-  platform=azure,
+  platform: 'azure',
   endpoint: 'https://dev.azure.com/ajitddarade/',
   token: process.env.TOKEN,
   hostRules: [
-    {
-      hostType: 'npm',
-      matchHost: 'pkgs.dev.azure.com',
-      username: 'ajitddarade',
-      password: process.env.TOKEN,
+    {           
+      "azureAutoApprove": true,
+      "automerge": true
     },
   ],
-  repositories: ['snyk-demo123/dummy-packages-codeql-dependabot--1'],
+  repositories: ['snyk-demo123/dummy-packages-codeql-dependabot--1'], // list of repos that should be watched by renovate bot
 };
